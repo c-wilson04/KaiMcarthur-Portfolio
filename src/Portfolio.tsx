d8800b0ea06f20c76bbebcd9cf95be8d3f3ede45
c-wilson04 <141252422+c-wilson04@users.noreply.github.com>
2023-12-27 14:52:11 -0500

    Update Portfolio.tsx
    missing slash

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -31,7 +31,7 @@ const Portfolio = () => {
                   height={250}
                 ></Image>
               </a>
-              <a href="images/Elantra1.jpeg" className="gallery-item">
+              <a href="/images/Elantra1.jpeg" className="gallery-item">
                 <Image
                   className="gallery-item"
                   src="images/Elantra1.jpeg"
